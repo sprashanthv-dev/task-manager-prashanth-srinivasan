d36b439ea12aaaca4509ade5d886d8d7b0e7883a
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-09-21 16:18:55 -0700

    feat: Header + Add Task Button + Task list components setup

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Welcome to Task Manager!</h1>
+      <h2 className="header">Task Manager</h2>
+      <button className="new-task-button">New Task</button>
     </>
   );
 }
---
+++ task-manager/src/components/TaskList.tsx
@@ -0,0 +1,5 @@
+const TaskList = () => {
+  return <></>;
+};
+
+export default TaskList;
